c6b61702bc43391353c33a3303c45b439de80adb
Compagnoni <ac22qawo@idiv.de>
2019-03-08 11:58:56 +0100

    Fix (almost!) broken loop, add minor stilistic changes

--- R/matrix _query_M_itried.R
+++ R/matrix _query_M_itried.R
@@ -1,52 +1,82 @@
 source('R/subset_orchids.R')
+library(dplyr)
 
-sub_ind_unmanipulated <- which(orchids$metadata$MatrixTreatment == 'Unmanipulated')
+# Shortening object names is very useful for readability:
+cntr_i <- which(orchids$metadata$MatrixTreatment == 'Unmanipulated')
 
-orchids_unmanipulated <- list(metadata = orchids$metadata[sub_ind_unmanipulated, ],
-                matrixClass = orchids$matrixClass[sub_ind_unmanipulated],
-                mat = orchids$mat[sub_ind_unmanipulated])
+orchids_unmanipulated <- list(metadata    = orchids$metadata[cntr_i, ],
+                              matrixClass = orchids$matrixClass[cntr_i],
+                              mat         = orchids$mat[cntr_i])
 
+means_i <- c(which(orchids_unmanipulated$metadata$MatrixComposite == "Mean"),
+             which(orchids_unmanipulated$metadata$MatrixComposite == "Pooled"))
 
-sub_ind_mean_pooled <- c(which(orchids_unmanipulated$metadata$MatrixComposite == "Mean"), which(orchids_unmanipulated$metadata$MatrixComposite == "Pooled"))
-
-orchids_mean_pooled <- list(metadata = orchids_unmanipulated$metadata[sub_ind_mean_pooled, ],
-                 matrixClass = orchids_unmanipulated$matrixClass[sub_ind_mean_pooled],
-                 mat = orchids_unmanipulated$mat[sub_ind_mean_pooled])
+orchids_mean_pooled <- list(metadata = orchids_unmanipulated$metadata[mean_i, ],
+                            matrixClass = orchids_unmanipulated$matrixClass[mean_i],
+                            mat = orchids_unmanipulated$mat[mean_i])
 
 # Selecting only matrices that are means of multiple populations
 orchids_mean_pooled$metadata[orchids_mean_pooled$metadata$MatrixPopulation %>% grep(";",.),]$MatrixPopulation
 
-saveRDS(orchids_mean_pooled, file = 'Data/Serialized/Compadre/many_orchids.rds')
+saveRDS(orchids_mean_pooled,
+        file = 'Data/Serialized/Compadre/many_orchids.rds')
 
+# find species for which you need to average individual matrices
 
+# exclude the species
+mean_spp <- orchids_mean_pooled %>%
+              .$metadata %>%
+              .$SpeciesAuthor %>%
+              .[sub_ind_mean_pooled] %>%
+              unique
 
+all_spp <- orchids_unmanipulated %>%
+              .$metadata %>%
+              .$SpeciesAuthor %>%
+              unique
 
+# spp the need be averaged
+avr_spp <- setdiff(all_spp, mean_spp)
 
+# extract ids (NOTE %in% operator)
+avr_i      <- which(orchids_unmanipulated$metadata$SpeciesAuthor %in% avr_spp)
+
+orchid_avg <- list(metadata    = orchids_unmanipulated$metadata[avr_i, ],
+                   matrixClass = orchids_unmanipulated$matrixClass[avr_i],
+                   mat         = orchids_unmanipulated$mat[avr_i])
 
 (orchidsi$mat[[1]]$matA + orchidsi$mat[[2]]$matA + orchidsi$mat[[3]]$matA)/3
 
 print(length(orchids$metadata$SpeciesAuthor[spp_i]))
 
+
+# list to stored mean matrices
 mean_mat_l <- list()
 
 # cycle through the species
-for(spp_i in 1:length(orchids$metadata$SpeciesAuthor)){
+for(spp_i in 1:length(orchid_avg$metadata$SpeciesAuthor)){
 
-  # the id of each species specific matrices
-  id_spp <- sum(orchidsi$metadata$SpeciesAuthor == orchidsi$metadata$SpeciesAuthor[spp_i])
+  # number of species-specific matrices
+  spp_n    <- sum(orchid_avg$metadata$SpeciesAuthor ==
+                  orchid_avg$metadata$SpeciesAuthor[spp_i])
+
+  # id of each species specific matrices
+  spps_ids <- which(orchid_avg$metadata$SpeciesAuthor ==
+                    orchid_avg$metadata$SpeciesAuthor[spp_i])
 
   # set up matrix to get the mean
   # start with the first matrix
-  mat_base <- orchidsi$mat[[id_spp[1]]]$matA
+  mat_base <- orchid_avg$mat[[id_spp[1]]]$matA
 
   # add matrices up
-  for(mat_i in 2:id_spp){
+  for(mat_i in 2:spp_n){
 
-    mat_sum <- mat_base  +  orchidsi$mat[[id_spp[[mat_i]]]]$matA
+    mat_base <- mat_base + orchid_avg$mat[[spps_ids[mat_i]]]$matA
 
   }
 
-  mean_mat_l[[spp_i]] <- mat_sum / length(spp_i)
+  mean_mat_l[[spp_i]] <- mat_base / spp_n
+
 }
 
 
