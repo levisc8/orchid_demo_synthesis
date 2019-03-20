2b70836e6a815425be5905129cb3fe154dcc2282
levisc8 <levisc8@gmail.com>
2019-03-21 00:09:09 +0100

    update pipeline - should work for everyone now

--- R/demographic_pipeline.R
+++ R/demographic_pipeline.R
@@ -1,9 +1,10 @@
 # Complete pipeline for deriving demographics. To add additional matrices, use
 # R/format_raw_matrix.R to update the the use_db created by matrix_query.R
 
+source('R/dependencies.R')
 source('R/subset_orchids.R')
-source('R/derive_demographics.R') # NEEDS EDITING
-
+source_w_path('R/format_raw_matrix.R', 'stash') # change "stash" to "wherever/you/stored/the/excel/files"
+source('R/derive_demographics.R')
 source('R/LH_PCA.R') # WRITE ME
 
 source('R/produce_figures.R')  # WRITE ME
