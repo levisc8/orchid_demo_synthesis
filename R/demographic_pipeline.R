e75e8b34b80379a6447ce47ec55e29fee363b1c4
SanneE1 <sanne.m.evers@gmail.com>
2019-03-21 17:20:21 +0100

    Correcting Pipeline

--- R/demographic_pipeline.R
+++ R/demographic_pipeline.R
@@ -1,18 +1,12 @@
 # Complete pipeline for deriving demographics. To add additional matrices, use
 # R/format_raw_matrix.R to update the the use_db created by matrix_query.R
 
-### if all new .xlsx files are stored
-source('R/subset_orchids.R')       ## get Orchids from COMPADRE
-source('R/format_raw_matrix.R')    ## add new Orchids from local files
-
-
-### if only .rds object is available
-# orchids <- readRDS('Data/Serialized/Compadre/compadre_plus_new_studies.rds')
-
-### need to select appropriate matrices (means, single or multiple populations etc.)
-
-source('R/derive_demographics.R') # NEEDS EDITING
-
+source('R/dependencies.R')
+source('R/subset_orchids.R')
+source_w_path('R/format_raw_matrix.R', 'stash') # change "stash" to "wherever/you/stored/the/excel/files"
+source('R/derive_demographics.R')
 source('R/LH_PCA.R') # WRITE ME
 
 source('R/produce_figures.R')  # WRITE ME
+
+
