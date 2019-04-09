15d390e11fd63a2e4fa3a53949c513511e3371d9
SanneE1 <sanne.m.evers@gmail.com>
2019-04-09 15:44:50 +0200

    Phylogenetic figures with demographic rates and fix for calling old compadre

--- R/demographic_pipeline.R
+++ R/demographic_pipeline.R
@@ -3,9 +3,10 @@
 
 source('R/dependencies.R')
 source('R/subset_orchids.R')
-source_w_path('R/format_raw_matrix.R', 'stash/Orchid New Matrices') # change "stash" to "wherever/you/stored/the/excel/files"
+# source_w_path('R/format_raw_matrix.R', 'stash/Orchid New Matrices') # change "stash" to "wherever/you/stored/the/excel/files"
 
 # orchids <- readRDS('Data/Serialized/Compadre/compadre_plus_new_studies.rds')
+source('R/hand_selected_orchids.R')
 source('R/derive_demographics.R')
 source('R/LH_PCA.R') # WRITE ME
 
--- R/produce_figures.R
+++ R/produce_figures.R
@@ -82,7 +82,6 @@ USOrchids <- wrld_base +
 
 
 
-
 #############################
 ####     Demographics    ####
 #############################
@@ -131,7 +130,7 @@ R0dismean <- ggplot(mu,
   xlim(0, 13)+
   ylim(0, 1.1)
 
-#### Boxplot ####
+#### Boxplot
 
 boxR0 <- ggplot(mu,
                 aes(fill = GrowthForm)) +
@@ -140,3 +139,126 @@ boxR0 <- ggplot(mu,
   theme(axis.ticks.x=element_blank(),
         axis.line = element_line(color="black", size = 1)
   )
+
+
+
+
+############################################
+####     Phylos w/ Demographic rates    ####
+############################################
+
+#### Part of the make_Phylos that is needed, make_Phylos still calls for original COMAPDRE (without added orchids) ####
+
+fuzz_tree <- readRDS('Data/Serialized/Phylogenies/all_compadre_bien_fuzzy_matched.RDS')
+
+mol_tree <- readRDS('Data/Serialized/Phylogenies/molecular_bien_tree.rds')
+
+Compadre <- orchids
+
+all_orc_occ <- readRDS('Data/Serialized/BIEN/Occurences.rds')
+
+all_orc_names <- unique(all_orc_occ$scrubbed_species_binomial) %>%
+  gsub(' ', '_', .)
+
+orc_compadre <- grepl('Orchidaceae', Compadre$metadata$Family)
+
+orc_compadre_spp <- unique(Compadre$metadata$SpeciesAccepted[orc_compadre]) %>%
+  gsub(' ', '_', .)
+
+fuzz_tree <- drop.tip(fuzz_tree, setdiff(fuzz_tree$tip.label, all_orc_names))
+fuzz_groups <- split(fuzz_tree$tip.label, fuzz_tree$tip.label %in% orc_compadre_spp)
+fuzz_tree <- groupOTU(fuzz_tree, fuzz_groups)
+fuzz_tree$tip.label[!fuzz_tree$tip.label %in% orc_compadre_spp] <- ""
+
+fuzz_plot <- ggtree(fuzz_tree,
+                    aes(color = group),
+                    layout = 'rectangular') +
+  geom_tippoint(aes(color = group)) +
+  scale_color_manual('',
+                     breaks = c(TRUE, FALSE),
+                     labels = c("In Compadre",
+                                "Not in Compadre"),
+                     values = c('red', 'black')) +
+  theme(legend.position = 'right') +
+  ggtitle("Fuzzily matched, not completely molecular tree")
+
+
+
+
+####
+#### Only our orchids and their relation (on genus level) ####
+our_fuzz_tree <- fuzz_tree
+our_fuzz_tree$tip.label <- sub("_.*", "", our_fuzz_tree$tip.label)
+our_fuzz_tree <- drop.tip(our_fuzz_tree, setdiff(our_fuzz_tree$tip.label, sub("_.*", "", orc_compadre_spp)))
+our_fuzz_tree <- drop.tip(our_fuzz_tree, our_fuzz_tree$tip.label[c(which(duplicated(our_fuzz_tree$tip.label)))])
+
+
+fuzz_our_orchids <- ggtree(our_fuzz_tree,
+                           layout = 'rectangular') +
+  geom_tiplab() +
+  xlim_tree(60) +
+  theme_tree2()
+
+
+####
+#### with demographic rates ####
+a <- data.frame(sub(" .*", "", output$SpeciesAccepted), output$GrowthForm, output$R0, output$GenT, output$Lambda)
+names(a) <- c("Genus", "GrowthForm", "R0", "GenT", "Lambda")
+a <- a[which( a$Genus %in% our_fuzz_tree$tip.label ),]
+
+facet_plot(fuzz_our_orchids, panel = "R0", data = a, geom = geom_point, mapping = aes(x= R0))
+facet_plot(fuzz_our_orchids, panel = "GenT", data = a, geom = geom_point, mapping = aes(x= GenT))
+facet_plot(fuzz_our_orchids, panel = "Lambda", data = a, geom = geom_point, mapping = aes(x= Lambda))
+
+d=data.frame(y=1:8, .panel='Lambda')
+e=data.frame(y=1:8, .panel= "GenT")
+f=data.frame(y=1:8, .panel= "R0")
+
+Lambda_tree <- facet_plot(fuzz_our_orchids,
+                          panel = "Lambda",
+                          data = a,
+                          geom = geom_point,
+                          mapping = aes(x= Lambda, color = GrowthForm, size = .5)
+) +
+  geom_hline(data = d, aes(yintercept = y))
+
+GenT_tree <-facet_plot(fuzz_our_orchids,
+                       panel = "GenT",
+                       data = a,
+                       geom = geom_point,
+                       mapping = aes(x= GenT, color = GrowthForm, size = .5)) +
+  geom_hline(data = e, aes(yintercept = y))
+
+R0_tree <- facet_plot(fuzz_our_orchids,
+                      panel = "R0",
+                      data = a,
+                      geom = geom_point,
+                      mapping = aes(x= R0, color = GrowthForm, size = .5)) +
+  geom_hline(data = f, aes(yintercept = y))
+
+
+
+L_GenT_tree <- facet_plot(Lambda_tree,
+                          panel = "GenT",
+                          data = a,
+                          geom = geom_point,
+                          mapping = aes(x= GenT, color = GrowthForm, size = .5)) +
+  geom_hline(data = e, aes(yintercept = y))
+
+LGR_tree <- facet_plot(L_GenT_tree,
+                       panel = "R0",
+                       data = a,
+                       geom = geom_point,
+                       mapping = aes(x= R0, color = GrowthForm, size = .5)) +
+  geom_hline(data = f, aes(yintercept = y))
+
+
+####
+#### plot ####
+
+Lambda_tree
+GenT_tree
+R0_tree
+LGR_tree
+
+
